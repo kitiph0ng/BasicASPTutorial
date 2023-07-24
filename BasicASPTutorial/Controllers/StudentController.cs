3584cbe320ca9915c0afb0e9c77998dcd642abab
kitiph0ng <kitiphong1990@gmail.com>
2023-07-25 01:41:13 +0700

    เรียนการสร้าง controller

---
+++ BasicASPTutorial/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BasicASPTutorial.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+	        return Content("คะแนนสอบวิชาคอมพิวเตอร์");
+        }
+
+        public IActionResult Detail() {
+            return Content("รายละเอียดคะแนนสอบ");
+        }
+
+        public IActionResult ShowScore(int id) {
+            return Content($"คะแนนสอบของนักเรียนเลขที่ {id} : {new Random().NextInt64(0, 100)}");
+        }
+    }
+
+}
+
