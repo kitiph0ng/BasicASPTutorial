656ed48b0188336bb0467ac8b452f6e1812be8fa
kitiph0ng <kitiphong1990@gmail.com>
2023-07-25 00:47:13 +0700

    New Project

---
+++ BasicASPTutorial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicASPTutorial.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
